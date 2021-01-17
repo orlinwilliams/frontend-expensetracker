4aad8d4c87f1084aaa4e3a7cc5213c85a3f002d5
orlinwilliams <orlinwilliams0077@gmail.com>
2021-01-17 15:31:41 -0600

    layout login

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthenticationComponent } from './authentication.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [{ path: '', component: AuthenticationComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthenticationComponent,
+    children: [
+      { path: '', component: LoginComponent },
+      { path: 'register', component: LoginComponent },
+    ],
+  },
+  //{ path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthenticationRoutingModule { }
+export class AuthenticationRoutingModule {}
