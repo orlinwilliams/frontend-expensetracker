aa67b3323bba6a6071208288cda041da920c7da6
orlinwilliams <orlinwilliams0077@gmail.com>
2021-01-17 15:57:28 -0600

    starting dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,11 @@ const routes: Routes = [
         (m) => m.AuthenticationModule
       ),
   },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
 ];
 
 @NgModule({
