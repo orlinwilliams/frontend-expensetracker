ef0e0378a9c17ac3abddc3188903267bfbbc5bab
orlinwilliams <orlinwilliams0077@gmail.com>
2021-01-19 19:58:28 -0600

    finished table

--- src/app/dashboard/expenses/expenses.component.ts
+++ src/app/dashboard/expenses/expenses.component.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-//import { faEdit, faTrashAlt, } from '@fortawesome/free-solid-svg-icons';
 import { faTrashAlt, faEdit } from '@fortawesome/free-regular-svg-icons';
 @Component({
   selector: 'app-expenses',
--- src/app/dashboard/income/income.component.ts
+++ src/app/dashboard/income/income.component.ts
@@ -1,19 +1,20 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
-
+import { faTrashAlt, faEdit } from '@fortawesome/free-regular-svg-icons';
 @Component({
   selector: 'app-income',
   templateUrl: './income.component.html',
   styleUrls: ['./income.component.css']
 })
 export class IncomeComponent implements OnInit, AfterViewInit {
-
-  displayedColumns: string[] = ['position', 'date', 'category', 'value'];
+  faEdit = faEdit;
+  faTrashAlt = faTrashAlt;
+  displayedColumns: string[] = ['date', 'category', 'value', 'actions'];
   DATA = [
-    { position: 1, date: 'Hydrogen', category: 1.0079, value: 'H' },
-    { position: 2, date: 'Hydrogen', category: 2.025, value: 'M' },
-    
+    { date: 'Cloro', category: 1.0079, value: 'H' },
+    { date: 'Oxigen', category: 3.025, value: 'M' },
+    { date: 'Hydrogen', category: 2.025, value: 'M' },
   ];
   dataSource = new MatTableDataSource(this.DATA);
   @ViewChild(MatSort) sort: MatSort = new MatSort();
